f9eda54267f8446dc24c3a28f62b45a3613008ba
duncanobrien <duncan.a.obrien@gmail.com>
2022-02-21 16:42:20 +0000

    improved annotations

--- Code/community_analyses.R
+++ Code/community_analyses.R
@@ -4,13 +4,15 @@
 
 ### Preamble ###
 library(tidyverse) # data wrangling and plotting
-library(vegan)
+library(vegan) # community ecology metrics and analysis
+library(ggrepel) # non-overlapping labels
+library(ggpubr) # multi-panelled plotting
 library(FactoMineR)
 library(factoextra)
 
-source("Code/data_preparation.R")
+source("Code/data_preparation.R") # load raw data
 
-colnames(data_pooled)[6:12] <- c("Blepharisma","Didinium","Colpidium","Homolazoon","Bursaria","Spirostomum","Paramecium") #rename species for plotting
+colnames(data_pooled)[6:12] <- c("Blepharisma","Didinium","Colpidium","Homolazoon","Bursaria","Spirostomum","Paramecium") #rename species for downstream plotting
 data_pooled_lastday <- data_pooled %>%
   filter(NumDays ==max(NumDays)) #filter to last day of experiment
 
@@ -23,40 +25,41 @@ data_pooled_half <- data_pooled %>%
 
 #week_four_microcosm_PCA <- FactoMineR::PCA(data_pooled_lastday[,c(6:12)],scale.unit = TRUE, graph = FALSE)
 week_four_microcosm_PCA <- prcomp(data_pooled_lastday[,c(6,8:12)],scale. = T) #estimate PCs using base R prcomp (scaled and centered)
+#Didinium dropped due to all zeroes across treatments and inability to scale when variance is constant
 
 summary(week_four_microcosm_PCA) # PCs 1 & 2  contribute ~34 and 19% variation respectively
 
 pca_lastday_df <- data.frame(PC1 = week_four_microcosm_PCA$x[,1], #extract replicate positions and first two PC axes
                              PC2 = week_four_microcosm_PCA$x[,2], 
-                             data_pooled_lastday) %>%
+                             data_pooled_lastday) %>% # combine with original dataset
   left_join(aggregate(cbind(PC1.centroid = week_four_microcosm_PCA$x[,1],PC2.centroid = week_four_microcosm_PCA$x[,2])~Temp_Regime, 
-                      data =data_pooled_lastday, mean)) #extract centroid positions (mean)
+                      data =data_pooled_lastday, mean)) #extract centroid positions (mean) for plotting (if desired)
 
 
 pca_lastday_vars <- data.frame(PC1 = week_four_microcosm_PCA$rotation[,1]*5, #extract species contributions and multiply by 5 for prominent arrows
                                PC2 =  week_four_microcosm_PCA$rotation[,2]*5,
-                               species = rownames(week_four_microcosm_PCA$rotation))
+                               species = rownames(week_four_microcosm_PCA$rotation)) # label contributions with associated species
 
-#recreate fviz_pca_biplot as raw ggplot
+#recreate fviz_pca_biplot as raw ggplot. Options provided for either 95% multinormal ellipses or convex hull
 pca_lastday.plot <- ggplot(pca_lastday_df, aes(x = PC1,y=PC2))  + 
-  geom_vline(xintercept = 0,linetype = "dashed",col="black",alpha=0.8)+
+  geom_vline(xintercept = 0,linetype = "dashed",col="black",alpha=0.8)+ # lines denoting community space quadrants
   geom_hline(yintercept = 0,linetype = "dashed",col="black",alpha=0.8)+
   # geom_point(aes(alpha = Corridor,fill=Temp_Regime,col=Temp_Regime),shape = 21,size = 3, position=position_jitter(0.8,seed=123)) + # points for Long Corridors
   # geom_point(aes(col=Temp_Regime),shape = 21,size = 3, position=position_jitter(0.8,seed=123)) + # points for Short Corridors
   geom_point(aes(alpha = Corridor,fill=Temp_Regime,col=Temp_Regime),shape = 21,size = 3) + # points for Long Corridors
   geom_point(aes(col=Temp_Regime),shape = 21,size = 3) + # points for Short Corridors
-  geom_point(aes(x = PC1.centroid, y= PC2.centroid,fill=Temp_Regime,col=Temp_Regime),shape = 8,size = 5) + # points for Long Corridors
-  #stat_ellipse(aes(fill=Temp_Regime,col=Temp_Regime), alpha=.2,type='t',size =0.3, geom="polygon",level = 0.95)+ # 95% multinormal ellipses 
-  ggpubr::stat_chull(aes(fill=Temp_Regime,col=Temp_Regime), alpha = 0.1, 
-             geom = "polygon")+
+  stat_ellipse(aes(fill=Temp_Regime,col=Temp_Regime), alpha=.2,type='t',size =0.3, geom="polygon",level = 0.95)+ # 95% multinormal ellipses 
+  #geom_point(aes(x = PC1.centroid, y= PC2.centroid,fill=Temp_Regime,col=Temp_Regime),shape = 8,size = 5) + # points for Temp_Regime centroids
+  # ggpubr::stat_chull(aes(fill=Temp_Regime,col=Temp_Regime), alpha = 0.1, 
+  #            geom = "polygon")+ # Temp_Regime convex hulls
   xlab("PC1 (34.7% var explained)") + ylab("PC2 (19.1% var explained)")+
   scale_alpha_manual(values = c(1,0),
-                     guide = guide_legend(override.aes = list(alpha = 1,fill = c("black",NA))))+
+                     guide = guide_legend(override.aes = list(alpha = 1,fill = c("black",NA))))+ # legend customisation of Long vs Short corridors
   geom_segment(data = pca_lastday_vars,aes(x = 0, y= 0, xend = PC1,yend=PC2),
                arrow = arrow(length=unit(0.15,"cm")),alpha=0.7)+ # add species contribution arrows
   #geom_text(data= pca_lastday_vars, aes(x=PC1, y=PC2, label=species), size = 3, vjust =1,color="black")+ # label arrows
-  ggrepel::geom_text_repel(data= pca_lastday_vars, aes(x=PC1, y=PC2, label=species), size = 3,color="black",direction = "y")+ # label arrows
-  xlim(-6.5,6.5)+ylim(-5,5)+ #square plot area for symmetry and unbiased interpretation
+  ggrepel::geom_text_repel(data= pca_lastday_vars, aes(x=PC1, y=PC2, label=species), size = 3,color="black",direction = "y")+ # label arrows and ensure minimal overlap in text
+  xlim(-6.5,6.5)+ylim(-5,5)+ #symmetrical plot area for unbiased interpretation
   theme_bw()
 
 # fviz_pca_biplot(week_four_microcosm_PCA,
@@ -83,19 +86,23 @@ pca_lastday.plot <- ggplot(pca_lastday_df, aes(x = PC1,y=PC2))  +
 #         aspect.ratio = 1)+
 #   labs(x = "PC1", y = "PC2")
 
+## ASSESS EFFECT OF TREATMENT ON FINAL COMMUNITY COMPOSITION ##
 pooled_lastday_dist <- vegan::vegdist(data_pooled_lastday[,c(6:12)],method = "bray") #estimate bray-curtis dissimilarity due high proportion of zeroes
 
-#perform PERMANOVA and PERMDIST
-adonis2(pooled_lastday_dist~data_pooled_lastday$Temp_Regime + data_pooled_lastday$Corridor,
-       data = data_pooled_lastday, method = "bray",by="margin") # no interaction included per other analyses
+#perform PERMANOVA and PERMDIST.
+vegan::adonis2(pooled_lastday_dist~data_pooled_lastday$Temp_Regime + data_pooled_lastday$Corridor,
+       data = data_pooled_lastday,by="margin") # no interaction included per other analyses
+#adonis2 preferred to adonis due to improved customisation and clarity. 
+#by = "margin" is marginal effect of each predictor rather than additional effect that
+#predictor has over previous. Therefore order of predictors supplied to model irrelevant
 
-## DEFINING `TYPE` AS CENTROID/MEDIAN ALTERS INTERPRETATION ##
-permutest(vegan::betadisper(pooled_lastday_dist,data_pooled_lastday$Temp_Regime,type = "centroid"),pairwise = T) 
-#PERMDIST with pairwise comparisons. permutest vegan specific function with greater customisation
+#!!!DEFINING `TYPE` AS CENTROID/MEDIAN ALTERS INTERPRETATION!!!# Needs discussing
+vegan::permutest(vegan::betadisper(pooled_lastday_dist,data_pooled_lastday$Temp_Regime,type = "centroid"),pairwise = T) #compare variance of Temp_Regime groups 
+#PERMDIST with pairwise comparisons. permutest = vegan specific function with greater customisation
 #dispersions are heterogeneous due Constant:Fluctuating_Synchro
 #TukeyHSD(vegan::betadisper(pooled_lastday_dist,data_pooled_lastday$Temp_Regime,type = "centroid")) #agrees
 
-permutest(vegan::betadisper(pooled_lastday_dist,data_pooled_lastday$Corridor,type = "centroid"),pairwise = T) 
+vegan::permutest(vegan::betadisper(pooled_lastday_dist,data_pooled_lastday$Corridor,type = "centroid")) #compare variance of Corridor groups
 #dispersions are homogeneous
 
 #----------------------------------------------------------------------------------------
@@ -153,14 +160,15 @@ pca_half.plot <- ggplot(pca_half_df, aes(x = PC1,y=PC2))  +
 #         aspect.ratio = 1)+
 #   labs(x = "PC1", y = "PC2")
 
+## ASSESS EFFECT OF TREATMENT ON INTERMEDIATE COMMUNITY COMPOSITION ##
 pooled_half_dist <- vegan::vegdist(data_pooled_half[,c(6:12)],method = "bray")
 #perform PERMANOVA and PERMDIST
 vegan::adonis2(pooled_half_dist~data_pooled_half$Temp_Regime + data_pooled_half$Corridor,
                data = data_pooled_half, method = "bray",by="margin") # no interaction per other analyses
-permutest(vegan::betadisper(pooled_half_dist,data_pooled_half$Temp_Regime,type = "centroid"),pairwise = T) #PERMDIST with pairwise comparisons 
+vegan::permutest(vegan::betadisper(pooled_half_dist,data_pooled_half$Temp_Regime,type = "centroid"),pairwise = T) #PERMDIST with pairwise comparisons 
 #TukeyHSD(vegan::betadisper(pooled_half_dist,data_pooled_half$Temp_Regime,type = "centroid")) #agrees
 
-permutest(vegan::betadisper(pooled_half_dist,data_pooled_half$Corridor,type = "centroid"),pairwise = T) 
+vegan::permutest(vegan::betadisper(pooled_half_dist,data_pooled_half$Corridor,type = "centroid"),pairwise = T) 
 #TukeyHSD(vegan::betadisper(pooled_half_dist,data_pooled_half$Corridor,type = "centroid"))
 #dispersions are hetergeneous
 
@@ -176,15 +184,17 @@ ggpubr::ggarrange(pca_half.plot,pca_lastday.plot,nrow = 2,ncol = 1,common.legend
 #----------------------------------------------------------------------------------------
 
 #generate prcs for corridor and temperature regime separately, and then for an interaction factor 
-#(prcs can currently only be performed on a single treatment)
-corridor.prc <- vegan::prc(log1p(data_pooled[,c(6:12)]),treatment = data_pooled$Corridor, as.factor(data_pooled$NumDays))
-temp_regime.prc <- vegan::prc(log1p(data_pooled[,c(6:12)]),treatment = data_pooled$Temp_Regime, as.factor(data_pooled$NumDays))
-interaction.prc <-  vegan::prc(log1p(data_pooled[,c(6:12)]),treatment = interaction(data_pooled$Corridor,data_pooled$Temp_Regime), as.factor(data_pooled$NumDays))
+#(prcs can currently only be performed on a single treatment). prc compares influence of each treatment
+#relative to a control over time by conditioning the fit on the time variable (i.e. NumDays)
+corridor.prc <- vegan::prc(log1p(data_pooled[,c(6:12)]),treatment = data_pooled$Corridor, as.factor(data_pooled$NumDays)) #control = Long corridors
+temp_regime.prc <- vegan::prc(log1p(data_pooled[,c(6:12)]),treatment = data_pooled$Temp_Regime, as.factor(data_pooled$NumDays)) #control = Constant temp_regime
+interaction.prc <-  vegan::prc(log1p(data_pooled[,c(6:12)]),treatment = interaction(data_pooled$Corridor,data_pooled$Temp_Regime), as.factor(data_pooled$NumDays)) #control = Long.Constant
+#multiple treatment factors aren't possible in standard prc, therefore explore interactions
 
 summary_temp_regime1 <- summary(temp_regime.prc, scaling = "symmetric", axis = 1,
                               correlation = F) #extract first component 
 summary_temp_regime2 <- summary(temp_regime.prc, scaling = "symmetric", axis = 2,
-                               correlation = F) #extract first component 
+                               correlation = F) #extract second component 
 summary_corridor1 <- summary(corridor.prc, scaling = "symmetric", axis = 1,
                               correlation = F)
 summary_corridor2 <- summary(corridor.prc, scaling = "symmetric", axis = 2,
@@ -200,10 +210,10 @@ anova(interaction.prc) # differences identified
 
 # visualise PRCs
 df_pc1 <- data.frame(NumDays = unique(data_pooled$NumDays), Short = t(coef(summary_corridor1)), Long = 0, Constant = 0) %>% 
-  cbind(t(coef(summary_temp_regime1))) %>%
-  pivot_longer(-NumDays,names_to = "treatment",values_to = "effect") %>%
-  mutate(Treatment_category = ifelse(treatment %in% c("Short","Long"),"Corridor","Temp_Regime"))%>%
-  mutate(PC = "PC1")
+  cbind(t(coef(summary_temp_regime1))) %>% #merge time data, control effects and estimated effects from prc
+  pivot_longer(-NumDays,names_to = "treatment",values_to = "effect") %>% # pivot to long format
+  mutate(Treatment_category = ifelse(treatment %in% c("Short","Long"),"Corridor","Temp_Regime"))%>% # classify into treatments
+  mutate(PC = "PC1") # classify associated principal component
   
 df_pc2 <- data.frame(NumDays = unique(data_pooled$NumDays), Short = t(coef(summary_corridor2)), Long = 0, Constant = 0) %>% 
   cbind(t(coef(summary_temp_regime2))) %>%
@@ -213,28 +223,29 @@ df_pc2 <- data.frame(NumDays = unique(data_pooled$NumDays), Short = t(coef(summa
 
 df_fin <- rbind(df_pc1,df_pc2)
 
-#visualise differences of treatments vs control treatment (Long corridor for corridor and Constant for temperature)
+#visualise estimated effect differences of treatments vs control treatment (Long corridor for corridor and Constant for temperature)
 ggplot(df_fin,aes(x=NumDays,y=effect)) + 
-  #geom_line(aes(linetype = Treatment_category)) + facet_wrap(~PC) +
-  geom_point(aes(col=treatment))+
-  geom_line(aes(col=treatment)) + facet_grid(PC~Treatment_category) + #combine both treatment types in to single figure
+  geom_point(aes(col=treatment))+ # points and...
+  geom_line(aes(col=treatment)) + # lines of effects through time
+  facet_grid(PC~Treatment_category) + #combine both treatment types in to single figure
   scale_color_manual(values = scales::hue_pal()(6),name = "Treatment") +
   theme_bw() + xlab("Day of experiment") + ylab("Effect") +
   geom_rug(data = rbind(data.frame(spp = names(summary_corridor1$sp),effect = summary_corridor1$sp*0.1, PC = "PC1",Treatment_category = "Corridor"),
                        data.frame(spp = names(summary_corridor1$sp),effect = summary_corridor1$sp*0.1, PC = "PC2",Treatment_category = "Corridor"),
                        data.frame(spp = names(summary_temp_regime1$sp),effect = summary_temp_regime1$sp*0.1, PC = "PC1",Treatment_category = "Temp_Regime"),
                        data.frame(spp = names(summary_temp_regime2$sp),effect = summary_temp_regime2$sp*0.1, PC = "PC2",Treatment_category = "Temp_Regime")),
-          sides = "r",outside = F, #geom_rug plots influence strength of species on second y axis
+          sides = "r",outside = F, #geom_rug plots influence strength of individual species on second y axis
           mapping = aes_string(group = NULL, x = NULL,
                                colour = NULL, linetype = NULL)) +
-  scale_y_continuous("Effect", sec.axis = sec_axis(trans = ~ .*10, name = "Species influence")) + #standardise second y axis
+  scale_y_continuous("Effect", sec.axis = sec_axis(trans = ~ .*10, name = "Species influence")) + # plot and standardise second y axis as magnitude of effects too large to fit on original (left hand) scale
   ggrepel::geom_text_repel(data= rbind(data.frame(spp = names(summary_corridor1$sp),effect = summary_corridor1$sp*0.1, PC = "PC1",Treatment_category = "Corridor"),
                                        data.frame(spp = names(summary_corridor1$sp),effect = summary_corridor1$sp*0.1, PC = "PC2",Treatment_category = "Corridor"),
                                        data.frame(spp = names(summary_temp_regime1$sp),effect = summary_temp_regime1$sp*0.1, PC = "PC1",Treatment_category = "Temp_Regime"),
                                        data.frame(spp = names(summary_temp_regime2$sp),effect = summary_temp_regime2$sp*0.1, PC = "PC2",Treatment_category = "Temp_Regime")),
-                           mapping=aes(x=32,y=effect,label=spp),hjust=-0.5, size=3, direction = "y",box.padding = 0.3,min.segment.length = 0.1,vjust=0.55) +#label species
-  xlim(0,32)
+                           mapping=aes(x=32,y=effect,label=spp),hjust=-0.5, size=3, direction = "y",box.padding = 0.3,min.segment.length = 0.1,vjust=0.55) + # label species and ensure minimal overlap
+  xlim(0,32) # extend y axis to fit labels
 
+#visualise estimated effect differences of interacting treatments vs control treatment (Long corridor.Constant temperature)
 df_interaction_pc1 <- data.frame(NumDays = unique(data_pooled$NumDays), Long.Constant =0) %>% 
   cbind(t(coef(summary_interaction1))) %>%
   pivot_longer(-NumDays,names_to = "treatment",values_to = "effect") %>%
@@ -248,7 +259,6 @@ df_interaction_pc2 <- data.frame(NumDays = unique(data_pooled$NumDays), Long.Con
 df_interaction_fin <- rbind(df_interaction_pc1,df_interaction_pc2)
 
 ggplot(df_interaction_fin,aes(x=NumDays,y=effect)) + 
-  #geom_line(aes(linetype = Treatment_category)) + facet_wrap(~PC) +
   geom_point(aes(col=treatment))+
   geom_line(aes(col=treatment)) + facet_wrap(~PC,strip.position = "right",dir="v") + 
   scale_color_manual(values = scales::hue_pal()(8),name = "Treatment") +
