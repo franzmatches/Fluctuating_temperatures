b448666c51a9e8cfe1d94c89dde1c0d0b8fe0867
Francesco Cerini <mr21762@bristol.ac.uk>
2022-08-12 12:59:42 +0100

    code for dispersal tests

---
+++ Code/Dispersal_tests_long_vs_short_corridors.R
@@ -0,0 +1,25 @@
+#########################################################################################
+### DISPERSAL TEST LONG VS SHORT CORRIDORS #####
+#########################################################################################
+
+#preable
+library(openxlsx)
+library(tidyverse) # data wrangling and plotting
+library(ggplot2)
+
+
+#load data
+data<-read.xlsx("Data/Dispersal_experiments_data.xlsx", sheet = "Prey_species", colNames = T)
+
+#perform Mann-Whitney-Wilcoxon Test
+wilcox.test(N_individuals_2h ~ Corridor,data = data %>% filter(Species == "P.caudatum"))
+wilcox.test(N_individuals_2h ~ Corridor,data = data %>% filter(Species == "B.japonicum"))
+wilcox.test(N_individuals_2h ~ Corridor,data = data %>% filter(Species == "S.teres"))
+wilcox.test(N_individuals_2h ~ Corridor,data = data %>% filter(Species == "Colpidium"))
+
+#plot boxplots to see difference between long and short facet species
+ggplot(data, aes (x = Corridor, y = N_individuals_2h, color = Species))+
+  geom_boxplot()+
+  facet_wrap(~Species, scales = "free")+
+  theme_bw()
+ggsave("dispersal_boxplots.pdf", units = "in", width = 10, height = 10)
